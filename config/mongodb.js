adc5fcd2da56462aefb5676779a6833bfb9de067
ferin <sudawan91@gmail.com>
2024-11-29 14:36:32 +0700

    backend

---
+++ config/mongodb.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", () => {
+            console.log("Connected to MongoDB ✅");
+        });
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log("MongoDB connection successful.");
+    } catch (error) {
+        console.error("Error connecting to MongoDB ❌:", error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
